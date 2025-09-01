b391ada0c1bb4303820a77bdb7a7e2d25a707927
kyrillos25 <kyrillos25@gmail.com>
2025-09-01 15:32:44 +0300

    Edit migration

--- src/Modules/WFManagment/WorkflowTracking.Modules.WFManagment.Infrastructure/Database/Migrations/20250901122654_EditRelation.Designer.cs
+++ src/Modules/WFManagment/WorkflowTracking.Modules.WFManagment.Infrastructure/Database/Migrations/20250901122654_EditRelation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WorkflowTracking.Modules.WFManagment.Infrastructure.Database;
@@ -11,9 +12,11 @@ using WorkflowTracking.Modules.WFManagment.Infrastructure.Database;
 namespace WorkflowTracking.Modules.WFManagment.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(WorkflowsDbContext))]
-    partial class WorkflowsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250901122654_EditRelation")]
+    partial class EditRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/WFManagment/WorkflowTracking.Modules.WFManagment.Infrastructure/Database/Migrations/20250901122654_EditRelation.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkflowTracking.Modules.WFManagment.Infrastructure.Database.Migrations;
+
+/// <inheritdoc />
+public partial class EditRelation : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "fk_workflow_steps_workflows_workflow_id",
+            schema: "workflows",
+            table: "workflow_steps");
+
+        migrationBuilder.AddForeignKey(
+            name: "fk_workflow_steps_workflows_workflow_id",
+            schema: "workflows",
+            table: "workflow_steps",
+            column: "workflow_id",
+            principalSchema: "workflows",
+            principalTable: "workflows",
+            principalColumn: "id",
+            onDelete: ReferentialAction.Cascade);
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "fk_workflow_steps_workflows_workflow_id",
+            schema: "workflows",
+            table: "workflow_steps");
+
+        migrationBuilder.AddForeignKey(
+            name: "fk_workflow_steps_workflows_workflow_id",
+            schema: "workflows",
+            table: "workflow_steps",
+            column: "workflow_id",
+            principalSchema: "workflows",
+            principalTable: "workflows",
+            principalColumn: "id");
+    }
+}
